a858a859dbfc7770f1db320066812c15e946b91c
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-04-28 17:12:25 -0300

    Realiza a inclusão do serviço para consumo de marcas

---
+++ src/DesafioWebMotors.ApiServices/DataTransferObjects/Marca.cs
@@ -0,0 +1,9 @@
+﻿
+namespace DesafioWebMotors.ApiServices.DataTransferObjects
+{
+    public class Marca
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/DesafioWebMotors.ApiServices/Services/OnlineChallengeApiService.cs
@@ -0,0 +1,32 @@
+﻿using RestSharp;
+using System.Collections.Generic;
+using DesafioWebMotors.ApiServices.DataTransferObjects;
+
+namespace DesafioWebMotors.ApiServices.Services
+{
+    public class OnlineChallengeApiService
+    {
+        private string _baseUrl => "https://desafioonline.webmotors.com.br/api/OnlineChallenge/";
+
+        private RestClient _restClient;
+
+        public OnlineChallengeApiService()
+        {
+            _restClient = new RestClient(_baseUrl);
+        }
+
+        public IEnumerable<Marca> GetMarcas()
+        {
+            var marcas = new List<Marca>();
+            var request = new RestRequest("Make", Method.GET);
+            var response = _restClient.Execute<List<Marca>>(request);
+
+            if (response.IsSuccessful)
+            {
+                marcas = response.Data;
+            }
+
+            return marcas;
+        }
+    }
+}
