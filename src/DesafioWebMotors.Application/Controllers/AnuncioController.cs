5d217825c2bac5b69d44f60774aabaa00ed8ab12
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-04-28 18:35:58 -0300

    Realiza listagem de anúncios e inclusão de funcionalidade básica de criação

--- src/DesafioWebMotors.Application/Controllers/AnuncioController.cs
+++ src/DesafioWebMotors.Application/Controllers/AnuncioController.cs
@@ -1,16 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using DesafioWebMotors.Application.Models;
+using DesafioWebMotors.Application.Services;
 
 namespace DesafioWebMotors.Application.Controllers
 {
     public class AnuncioController : Controller
     {
+        private readonly AnuncioService _anuncioService;
+
+        public AnuncioController(AnuncioService anuncioService)
+        {
+            _anuncioService = anuncioService;
+        }
+
         public IActionResult Index()
         {
-            var anuncios = new List<AnuncioWebMotors>();
+            var anuncios = _anuncioService.GetAnuncios();
 
             return View(anuncios);
+        }
+
+        public IActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new AnuncioWebMotors());
+            else
+                return View(_anuncioService.GetAnuncio(id));
         }
     }
 }
