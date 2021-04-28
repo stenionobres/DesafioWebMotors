aa324d15de210e33dcf80564c2d28b69628bce51
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-04-28 16:44:22 -0300

    Adiciona mapper para operações CRUD do anúncio

---
+++ src/DesafioWebMotors.Persistencia/Mappers/AnuncioWebMotorsMapper.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using DesafioWebMotors.Persistence.DataTransferObjects;
+using DesafioWebMotors.Persistence.EntityFrameworkContexts;
+
+namespace DesafioWebMotors.Persistence.Mappers
+{
+    public class AnuncioWebMotorsMapper
+    {
+        public AnuncioWebMotors Get(int id)
+        {
+            using (var context = new DesafioWebMotorsDbContext())
+            {
+                var anuncio = context.AnuncioWebMotors.Find(id);
+
+                return anuncio;
+            }
+        }
+
+        public IEnumerable<AnuncioWebMotors> GetAll()
+        {
+            using (var context = new DesafioWebMotorsDbContext())
+            {
+                var anuncios = context.AnuncioWebMotors.ToList();
+
+                return anuncios;
+            }
+        }
+
+        public void Save(AnuncioWebMotors anuncio)
+        {
+            using (var context = new DesafioWebMotorsDbContext())
+            {
+                context.AnuncioWebMotors.Add(anuncio);
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(AnuncioWebMotors anuncio)
+        {
+            using (var context = new DesafioWebMotorsDbContext())
+            {
+                context.AnuncioWebMotors.Update(anuncio);
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(AnuncioWebMotors anuncio)
+        {
+            using (var context = new DesafioWebMotorsDbContext())
+            {
+                context.AnuncioWebMotors.Remove(anuncio);
+                context.SaveChanges();
+            }
+        }
+    }
+}
