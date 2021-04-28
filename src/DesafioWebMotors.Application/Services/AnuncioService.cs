ed94782e9a9a417b237e79460f73c6ab231fd76c
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-04-28 18:33:52 -0300

    Adiciona classe de serviço para operações básicas no anúncio

---
+++ src/DesafioWebMotors.Application/Services/AnuncioService.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using DesafioWebMotors.Application.Models;
+using DesafioWebMotors.Persistence.Mappers;
+
+namespace DesafioWebMotors.Application.Services
+{
+    public class AnuncioService
+    {
+        private readonly AnuncioWebMotorsMapper _anuncioMapper;
+
+        public AnuncioService(AnuncioWebMotorsMapper anuncioMapper)
+        {
+            _anuncioMapper = anuncioMapper;
+        }
+
+        public IEnumerable<AnuncioWebMotors> GetAnuncios()
+        {
+            var anuncios = _anuncioMapper.GetAll();
+
+            return anuncios.Select(anuncioFromDatabase => new AnuncioWebMotors() 
+            {
+                Id = anuncioFromDatabase.Id,
+                Marca = anuncioFromDatabase.Marca,
+                Modelo = anuncioFromDatabase.Modelo,
+                Versao = anuncioFromDatabase.Versao,
+                Ano = anuncioFromDatabase.Ano,
+                Quilometragem = anuncioFromDatabase.Quilometragem,
+                Observacao = anuncioFromDatabase.Observacao
+            });
+        }
+
+        public AnuncioWebMotors GetAnuncio(int id)
+        {
+            return new AnuncioWebMotors();
+        }
+    }
+}
