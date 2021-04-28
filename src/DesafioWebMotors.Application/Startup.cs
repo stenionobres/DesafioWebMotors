1ae0525fdb455f6163257d7f715a9e663ffbfe4e
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-04-28 18:35:18 -0300

    Injeta dependência para serviço e mapeador de anúncio

--- src/DesafioWebMotors.Application/Startup.cs
+++ src/DesafioWebMotors.Application/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using DesafioWebMotors.Application.Services;
+using DesafioWebMotors.Persistence.Mappers;
 
 namespace DesafioWebMotors.Application
 {
@@ -24,6 +26,9 @@ namespace DesafioWebMotors.Application
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddTransient<AnuncioService>();
+            services.AddTransient<AnuncioWebMotorsMapper>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
