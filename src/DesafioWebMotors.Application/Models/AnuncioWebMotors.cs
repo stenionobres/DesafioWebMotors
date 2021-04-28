20af00aa0fbc7cfef9e6247572e4fac6595809ef
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-04-28 18:07:07 -0300

    Inclui campo de Id do anúncio

--- src/DesafioWebMotors.Application/Models/AnuncioWebMotors.cs
+++ src/DesafioWebMotors.Application/Models/AnuncioWebMotors.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿
 namespace DesafioWebMotors.Application.Models
 {
     public class AnuncioWebMotors
     {
+        public int Id { get; set; }
         public string Marca { get; set; }
         public string Modelo { get; set; }
         public string Versao { get; set; }
