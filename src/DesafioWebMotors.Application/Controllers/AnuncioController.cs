aca0f64b5038797d0c6d98ac130cdf80aa088348
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-04-28 18:08:04 -0300

    Cria estrutura básica de funcionalidade de listagem de anúncios

---
+++ src/DesafioWebMotors.Application/Controllers/AnuncioController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using DesafioWebMotors.Application.Models;
+
+namespace DesafioWebMotors.Application.Controllers
+{
+    public class AnuncioController : Controller
+    {
+        public IActionResult Index()
+        {
+            var anuncios = new List<AnuncioWebMotors>();
+
+            return View(anuncios);
+        }
+    }
+}
