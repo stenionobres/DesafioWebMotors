ea3fcd73c110752d4fb7bd20e1fb9342493661f1
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-04-28 22:41:25 -0300

    Inclui validações no model de anúncio

--- src/DesafioWebMotors.Application/Models/AnuncioWebMotors.cs
+++ src/DesafioWebMotors.Application/Models/AnuncioWebMotors.cs
@@ -1,15 +1,32 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace DesafioWebMotors.Application.Models
 {
     public class AnuncioWebMotors
     {
         public int Id { get; set; }
-        public string Marca { get; set; }
-        public string Modelo { get; set; }
-        public string Versao { get; set; }
-        public int Ano { get; set; }
-        public int Quilometragem { get; set; }
-        public string Observacao { get; set; }
 
+        [Required]
+        [MaxLength(45)]
+        public string Marca { get; set; }
+
+        [MaxLength(45)]
+        public string Modelo { get; set; }
+
+        [Required]
+        [MaxLength(45)]
+        public string Versao { get; set; }
+
+        [Required]
+        [Range(1900, int.MaxValue, ErrorMessage = "Por favor entre com um valor maior que {1}")]
+        public int Ano { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "Por favor entre com um valor maior que {1}")]
+        public int Quilometragem { get; set; }
+
+        [Required(ErrorMessage = "A observação é obrigatória")]
+        public string Observacao { get; set; }
+      
     }
 }
